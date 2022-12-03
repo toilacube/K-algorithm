9450157d8adcecc00df7dd798b549228c2d62bd4
toilacube <21520870@gm.uit.edu.vn>
2022-12-04 02:11:22 +0700

    Finish calculating density

--- Algorithm_2.py
+++ Algorithm_2.py
@@ -1,31 +1,49 @@
-from asyncio.windows_events import NULL
+
+# N # number of vertices aka number of nodes
+# k # number of clusters
+# cluster = []*N # for i in range(N): cluster[i] = clusterID where node i belongs to
+# graph = [[] for i in range(N)] # adjacent list to store graph
+# all_weight = [[] for i in range(k)] # all_weight[i][j] = sum of weight from node j
+# 								# to all the nodes within cluster i
+
+from Algorithm_3 import *
+
+def sorted_base_on_density(graph):
+
+    # Equation: dens(i) = multyplying the neighbor nod's total weight (Mj) by the edge weight(Wij) connecting to that neighbor +
+    dens = []
+
+    # Calculate density for each vertex
+    for vertex in graph.vertices: 
+        for neighbor in vertex.neighbors:
+            vertex.density += graph.weights[(vertex.id, neighbor.id)] * neighbor.weight
+            dens.append(vertex)
+    # Sort the vertex base on density
+    return sorted(dens, key = lambda vertex: vertex.density, reverse = True)
 
 
-N # number of vertices aka number of nodes
-k # number of clusters
-cluster = []*N # for i in range(N): cluster[i] = clusterID where node i belongs to
-graph = [[] for i in range(N)] # adjacent list to store graph
-all_weight = [[] for i in range(k)] # all_weight[i][j] = sum of weight from node j
-								# to all the nodes within cluster i
 
-def initialPartion(graph = [[]], k):
-# Line 1
-    for i in range(N):
-        cluster[i] = NULL
-    graph = sorted_based_on_density(graph)
-    seed = cluster_id = 1
-# Line 5
-    while cluster_id <= k and seed <= N:
-        while cluster[seed] not NULL:
-            seed += 1
+def initialPartion(graph, k):
+# # Line 1
+#     for i in range(N):
+#         cluster[i] = None
+#     graph = sorted_based_on_density(graph)
+#     seed = cluster_id = 1
+# # Line 5
+#     while cluster_id <= k and seed <= N:
+#         while cluster[seed] not None:
+#             seed += 1
 
-        cluster = growCluster(graph, cluster, cluster_id,seed, 0.8*(N/k)) # 80% node will be assign to a cluster
-        cluster_id += 1
+#         cluster = growCluster(graph, cluster, cluster_id,seed, 0.8*(N/k)) # 80% node will be assign to a cluster
+#         cluster_id += 1
 
-    for i in range(N): # the remaining 20% node will be chosen randomly
-        if cluster[i] == NULL:
-            cluster[i] = rand(1, k)
-    return cluster
+#     for i in range(N): # the remaining 20% node will be chosen randomly
+#         if cluster[i] == None:
+#             cluster[i] = rand(1, k)
+#     return cluster
     
+    graph_weights = graph.get_dict_of_weights() 
+    dens = sorted_base_on_density(graph)
+
 
 
