a36f53b6dd05f94b1ba499bab6ac6a6db555ee5e
toilacube <21520870@gm.uit.edu.vn>
2023-01-03 19:06:59 +0700

    adjust index range line 37

--- Algorithm_2.py
+++ Algorithm_2.py
@@ -9,42 +9,35 @@ def sorted_base_on_density(graph) -> list:
     # Calculate density for each vertex
     for vertex in graph.vertices: 
         for neighbor in vertex.neighbors:
-            vertex.density += graph.weights[(vertex.id, neighbor.id)] * neighbor.weight
-            dens.append(vertex)
+            vertex.density += graph.weights[(vertex.id, neighbor.id)] * neighbor.weight 
+        dens.append(vertex)
 
     # Sort the vertex base on density
-    return sorted(dens, key = lambda vertex: vertex.density, reverse = True)
-
-
+    return sorted(dens, key = lambda vertex: vertex.density, reverse = True) 
+    
 def initialPartion(graph):
-
     N = len(graph.vertices)
-
     dens = sorted_base_on_density(graph) # a list of vertex sorted base on density (Desc = True)
     seed_index = 0
-
     for i in range(graph.K):
-
         # select the seed that not in a cluster yet
-        while graph.vertex_in_cluster[dens[seed_index].id] is not None: 
+        while dens[seed_index].id in graph.vertex_in_cluster.keys() and seed_index < N - 1:
+        #while graph.vertex_in_cluster[dens[seed_index].id] is not None: 
             seed_index += 1
-
         if seed_index >= N:
             break
 
-        if graph.cluster[i] is None:
-            graph.cluster[i].id = i
-            graph.cluster[i] = growCluster( graph,
-                                            graph.cluster[i],
-                                            dens[seed_index], 
-                                            growSize = 0.8 * N /graph.K)
-        
-    for i in range(N, 0, -1):
+        graph.cluster[i] = Cluster()
+        graph.cluster[i].id = i
+        graph.cluster[i] = growCluster( graph,
+                                        graph.cluster[i],
+                                        dens[seed_index], 
+                                        growSize = int(0.8 * N /graph.K))
+
+    for i in range(N - 1, -1, -1): # for(int i = N - 1; i >= 0; i --)
         if dens[i].id not in graph.vertex_in_cluster.keys():
             j = random.randrange(0, graph.K)
-            graph.cluster[j].add_vertex(dens[i]) 
+            graph.cluster[j].add(dens[i], graph) 
             graph.vertex_in_cluster[dens[i].id] = graph.cluster[j] 
 
     return graph.cluster
- 
-
